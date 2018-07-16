b23431de026539efe682b35179b522bf48d9fd6b
August <mingxin.android@gmail.com>
2018-07-16 16:46:51 +0800

    m1/m1p/r1: uInitrd support and force fsck

--- sunxi-pack/chips/sun7i/configs/BPI-M1-M1P-R1/bananapi/bpi-m1/linux/boot.cmd
+++ sunxi-pack/chips/sun7i/configs/BPI-M1-M1P-R1/bananapi/bpi-m1/linux/boot.cmd
@@ -9,7 +9,7 @@ setenv kernel uImage.sun7i
 setenv script script.bin
 setenv verbosity 8
 #
-setenv bootargs "console=ttyS0,115200 console=tty1 board=${board} root=/dev/mmcblk0p2 rootwait rootfstype=ext4 cgroup_enable=memory swapaccount=1 hdmi.audio=EDID:0 disp.screen0_output_mode=1280x720p60 panic=10 consoleblank=0 enforcing=0 loglevel=${verbosity}"
+setenv bootargs "console=tty1 console=ttyS0,115200 board=${board} root=/dev/mmcblk0p2 rootwait rootfstype=ext4 fsck.mode=force fsck.repair=yes cgroup_enable=memory swapaccount=1 hdmi.audio=EDID:0 disp.screen0_output_mode=1280x720p60 panic=10 consoleblank=0 enforcing=0 loglevel=${verbosity}"
 #
 # load script.bin
 fatload $device ${partition} 0x43000000 ${bpi}/${board}/${service}/${script} || fatload $device ${partition} 0x43000000 script.bin 
@@ -18,9 +18,9 @@ fatload $device ${partition} 0x43000000 ${bpi}/${board}/${service}/${script} || 
 fatload $device ${partition} 0x48000000 ${bpi}/chip/${chip}/${kernel} || fatload $device ${partition} 0x48000000 uImage
 #
 # load initramdisk
-if fatload $device ${partition} 0x43100000 ${bpi}/uInitrd
+if fatload $device ${partition} 0x43300000 ${bpi}/chip/${chip}/uInitrd
 then
-bootm 0x48000000 0x43100000
+bootm 0x48000000 0x43300000
 else
 bootm 0x48000000
 fi
